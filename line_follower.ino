9eb0eaa9b2ceca672751a137ea4254e0b7c70201
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-04-24 21:20:18 +0200

    Add files via upload
    first declerations withut functions

---
+++ line_follower.ino
@@ -0,0 +1,62 @@
+--------------------------------------------------------------------------------------------
+//@project: line follower robot using esp32,IR sensors,DC motors,PCP
+//@authors:Ahmed Abdelhakem,Ahmed Osama,Mostafa Mohamed,Mazen Mohamed,Mohamed Ashraf
+//@date :24/4/2023
+--------------------------------------------------------------------------------------------
+
+//the IR sensors of the robot
+const int IR_1=VP;           //pin of first IR sensor
+const int IR_2=VN;          //pin of second IR sensor
+const int IR_3=D34;        //pin of third IR sensor
+const int IR_4=D35;        //pin of fourth IR sensor
+const int IR_5=D33;        //pin of fifth IR sensor
+
+//the values measured by IR sensors
+float val_1=0;             //value measured by first IR sensor
+float val_2=0;             //value measured by second IR sensor
+float val_3=0;            //value measured by third IR sensor
+float val_4=0;            //value measured by fourth IR sensor
+float val_5=0;            //value measured by fifth IR sensor
+
+//motor controles
+int MC_1_R=D21;
+int MC_1_l=D22;
+int MC_2_R=D23;
+int MC_2_l=D25;
+
+//direction of the movement
+char dir;
+
+
+void setup() {
+  pinMode(IR_1,INPUT);
+  pinMode(IR_2,INPUT);
+  pinMode(IR_3,INPUT);
+  pinMode(IR_4,INPUT);
+  pinMode(IR_5,INPUT);
+  pinMode(MC_1_R,OUTPUT);
+  pinMode(MC_1_l,OUTPUT);
+  pinMode(MC_2_R,OUTPUT);
+  pinMode(MC_2_l,OUTPUT);
+  Serial.begin(11600);
+  
+}
+
+void loop() {
+  val_1=digitalread(IR_1);   //read the digital value from the first sensor
+  val_2=digitalread(IR_2);   //read the digital value from the second sensor
+  val_3=digitalread(IR_3);   //read the digital value from the third sensor
+  val_4=digitalread(IR_4);   //read the digital value from the fourth sensor
+  val_5=digitalread(IR_5);   //read the digital value from the fifth sensor
+
+
+  //if conditions for the movement of the car
+  if((val_1)&&(val_2)&&(val_3)&&(val_4)&&(val_5)==1)
+  {
+    dir='f'
+    forward(dir)
+  }
+  }
+  
+
+}
