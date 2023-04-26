8c7158d9e9a5f2ada34116c16cbeeb8b82ab5efa
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-04-26 18:40:56 +0200

    Code demo with PID

--- CarNavigation/CarNavigation.ino
+++ CarNavigation/CarNavigation.ino
@@ -1,17 +1,95 @@
 #include "PID.h"
-PID pid_test;
-double rec = 0;
-double outPut = 0;
-void setup() {
-  // put your setup code here, to run once:
+#include "mDriver.h"
+
+// Motor pins
+#define AIN1 4
+#define BIN1 6
+#define AIN2 3
+#define BIN2 7
+#define PWMA 9
+#define PWMB 10
+
+// IR pins
+#define extremeLeft 1
+#define centerLeft 2
+#define center 3
+#define centerRight 4
+#define extremeRight 5
+
+// Calibration and normal modes' pins
+#define calibration 11
+#define normal 12
+
+// Correction factors for easily modifying motor configuration
+// line up with function names like forward.  Value can be 1 or -1
+const int correctionA = 1;
+const int correctionB = 1;
+
+// Initializing motors.  The library will allow you to initialize as many motors as you have memory for.
+Motor leftMotor = Motor(AIN1, AIN2, PWMA, correctionA);
+Motor rightMotor = Motor(BIN1, BIN2, PWMB, correctionB);
+
+// Motor speeds
+int lsp, rsp;
+int lfspeed = 200; // standard speed can be nodified later
+
+// PID constants
+float Kp = 0;
+float Kd = 0;
+float Ki = 0;
+// set-point variable
+int sp = 0;
+PID carPID(Kp, Ki, Kd); // PID object
+
+// color map values
+int minValues[6], maxValues[6], threshold[6];
+
+void setup()
+{
   Serial.begin(9600);
-  pid_test.set_constants(0,0.1,0);
-  pid_test.set_setPoint(100);
+  carPID.setSpeeds(lfspeed);
+  pinMode(calibration, INPUT_PULLUP);
+  pinMode(normal, INPUT_PULLUP);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  outPut = pid_test.calc(rec);
-  Serial.println(outPut);
-  rec++;
+
+void loop()
+{
+  // waiting for calibration
+  while (digitalRead(calibration)) {}
+  delay(1000);
+  carPID.calibrate(leftMotor, rightMotor, minValues, maxValues, threshold); // calibration mode
+  // waiting to be set to normal mode
+  while (digitalRead(normal)) {}
+  delay(1000);
+
+  // Normal mode in action
+  while (1)
+  {
+    // Extreme left turn when extremeLeft sensor detects dark region while extremeRight sensor detects white region
+    if (analogRead(extremeLeft) > threshold[1] && analogRead(extremeRight) < threshold[5] )
+    {
+      lsp = 0; rsp = lfspeed;
+      leftMotor.drive(0);
+      rightMotor.drive(lfspeed);
+    }
+
+    // Extreme right turn when extremeRight sensor detects dark region while extremeLeft sensor detects white region
+    else if (analogRead(extremeRight) > threshold[5] && analogRead(extremeLeft) < threshold[1])
+    { lsp = lfspeed; rsp = 0;
+      leftMotor.drive(lfspeed);
+      rightMotor.drive(0);
+    }
+    else if (analogRead(center) > threshold[3])
+    {
+      // arbitrary PID constans will be tuned later
+      Kp = 0.0006 * (1000 - analogRead(center));
+      Kd = 10 * Kp;
+      Ki = 0.0001;
+      carPID.setConstants(Kp, Ki, Kd);
+      sp = (analogRead(centerLeft) - analogRead(centerRight));
+      carPID.setSetpoint(sp);
+      carPID.linefollow(leftMotor, rightMotor, lsp, rsp);
+    }
+  }
 }
--- CarNavigation/PID.cpp
+++ CarNavigation/PID.cpp
@@ -1,51 +1,107 @@
 #include "PID.h"
-PID::PID(double kP, double kI, double kD)
+PID::PID(float kP, float kI, float kD)
 {
-  kp = kP;
-  ki = kI;
-  kd = kD;
-};
-
-void PID::set_constants(double kP, double kI, double kD)
-{
-  acc=0;
-  last_time=0;
   kp = kP;
   ki = kI;
   kd = kD;
 }
 
-void PID::set_setpoint(double setPoint)
+void PID::setSpeeds(int v)
 {
-  setpoint = setPoint;
+  lfspeed = v;
 }
 
-void PID::set_constrains(double lower_bound, double upper_bound)
+void PID::setConstants(float kP, float kI, float kD)
+{
+  kp = kP;
+  ki = kI;
+  kd = kD;
+}
+
+void PID::setSetpoint(int setPoint)
+{
+  sp = setPoint;
+}
+
+void PID::setConstrains(int lower_bound, int upper_bound)
 {
   lower = lower_bound;
   upper = upper_bound;
 }
 
-double PID::calc(double mv)
+int PID::calculate(int mv)
 {
-  if(!enabled) return 0;
-  double err = setpoint - mv;
-  double dt = (millis() - last_time)/1000.0;
 
+  P = mv;
+  I = I + mv;
+  D = mv - previousError;
 
-  double diff=0;
-  if(last_time)
-  {
-      diff = (mv-last_mv);
-      
-      diff/=dt;
-      acc+=err*dt;
-      acc=constrain(acc,lower,upper);
-
-  }
-  last_time = millis();
-  last_mv = mv;
+  PIDvalue = (kp * P) + (ki * I) + (kd * D);
  
-  return (err*kp)+(acc*ki)-(diff*kd);
+  return PIDvalue;
+}
 
+void PID::linefollow(Motor leftMotor, Motor rightMotor, int lsp, int rsp)
+{
+  int error = sp;
+
+  PIDvalue = calculate(error);
+  previousError = error;
+
+  lsp = lfspeed - PIDvalue;
+  rsp = lfspeed + PIDvalue;
+
+  if (lsp > 255) {
+    lsp = 255;
+  }
+  if (lsp < 0) {
+    lsp = 0;
+  }
+  if (rsp > 255) {
+    rsp = 255;
+  }
+  if (rsp < 0) {
+    rsp = 0;
+  }
+  leftMotor.drive(lsp);
+  rightMotor.drive(rsp);
+}
+
+
+void PID::calibrate(Motor leftMotor, Motor rightMotor, int minValues[], int maxValues[], int threshold[])
+{
+  for ( int i = 1; i < 6; i++)
+  {
+    minValues[i] = analogRead(i);
+    maxValues[i] = analogRead(i);
+  }
+  
+  for (int i = 0; i < 3000; i++)
+  {
+    leftMotor.drive(50);
+    rightMotor.drive(-50);
+
+    for ( int i = 1; i < 6; i++)
+    {
+      if (analogRead(i) < minValues[i])
+      {
+        minValues[i] = analogRead(i);     //minValues for white regions
+      }
+      if (analogRead(i) > maxValues[i])
+      {
+        maxValues[i] = analogRead(i);     //maxValues for dark regions
+      }
+    }
+  }
+
+  for ( int i = 1; i < 6; i++)
+  {
+    threshold[i] = (minValues[i] + maxValues[i]) / 2;
+    Serial.print(threshold[i]);
+    Serial.print("   ");
+  }
+  Serial.println();
+  
+  leftMotor.drive(0);
+  rightMotor.drive(0);
 }
